7cdd579bde5087bfe9fa82fa2accae195a90b829
Łukasz Basaj <lbasaj@gmail.com>
2018-01-16 12:18:27 +0100

    header code refactoring

---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import Typography from 'material-ui/Typography';
+
+class Header extends Component {
+    render() {
+        return (
+            <Typography type="display3"
+                        gutterBottom
+                        align="center"
+            >
+                My tasks
+            </Typography>
+        )
+    }
+}
+
+export default Header;
